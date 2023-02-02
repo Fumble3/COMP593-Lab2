5075911b0e4111f117564016c3a242ba2fd071c9
Mark Henderson <98548178+Fumble3@users.noreply.github.com>
2023-02-01 20:07:20 -0500

    Update COMP593Lab-2.py

--- COMP593Lab-2.py
+++ COMP593Lab-2.py
@@ -1,68 +1,52 @@
-
 def main():
-    
-    # Creating a complex data structure
-    about_me = {
-        'full_name': 'Mark Henderson',
+    # Create the complex data structure
+    data_table = {
+        'name': 'Mark Henderson',
         'student_id': 10204958,
         'pizza_toppings': [
-            'PEPPERONI',
-            'PINEAPPLE',
-            'WHITE ONION',
+            'Pepperoni',
+            'Pineapple',
+            'Bacon',
         ],
         'movies': [
-            {'movie1': 'Free Guy',
-                'title': 'Free Guy',
-                'genre': 'Comedy',
-                },
-                {'movie2': 'The Rundown',
-                    'title': 'The Rundown',
-                    'genre': 'Action Comedy',
-                },           
-            ]        
-    }
+            {'title': 'Get Hard',
+            'genre': 'Comedy',
+            },
+            {'title': 'Deadpool',
+            'genre': 'Comedy',
+            }
+        ]
+}
+
+    sentence(data_table)
     
-    print_student_name_and_id(about_me)
-    
-#Step 3 - Add another movie to the list of movies using the .append method
-    new_movie = {'movie3': 'The Hunger Games', 'title': 'The Hunger Games', 'genre': 'Suspenseful Thriller'}
-    about_me['movies'].append(new_movie)  
-    return
-    
-#Step 4 - Function that prints student name and ID	
-def print_student_name_and_id(about_me):
-    student_name = ('My name is ') + about_me['full_name'] + (' but you can call me Mr. ') + about_me['full_name'][5]
-    print(student_name)
-    
-    student_id = (about_me['student_id'])
-    print('My student ID is ', (student_id))
-    return
-    
-    
+
    
-# TODO: Step 5 - Function that adds pizza toppings to data structure
-def add_pizza_toppings(about_me, toppings):
+# Add one more movie to the list    
+    new_movie = {'title': 'The Hunger Games', 'genre': 'Suspenseful Thriller',}
+    data_table['movies'].append(new_movie)
     
+#Adding toppings to the list and sorting them
+def add_toppings(data_table, new_toppings):
     
+    new_toppings = ('Olives', 'Chicken', 'Onion')
+    data_table['pizza_toppings'].append(new_toppings)
     
-    return
+    sort_toppings = sorted(data_table['pizza_toppings']())
+    print(sort_toppings)
 
-
-
-
-
-# TODO: Step 6 - Function that prints bullet list of pizza toppings
-#def print_pizza_toppings(about_me):
- #   return
-
-# TODO: Step 7 - Function that prints comma-separated list of movie genres
-#def print_movie_genres(about_me):
- #   return 
-
-# TODO: Step 8 - Function that prints comma-separated list of movie titles
-#def print_movie_titles(movie_list):
- #   return
+def sentence(data_table):
     
+    print("My name is", data_table['name'],"but you can call me Mr.", data_table['name'][5])
+    print("My student ID is", data_table['student_id'])
+    print('My favourite pizza toppings are: ')
+    
+    for d in data_table['pizza_toppings']:
+        print(d)
+
+    print ('I like to watch', data_table['movies'],'movies.')
+   
+  
+
 if __name__ == '__main__':
     main()
-    
